d0446652dc92e386a77af40300ad19bee2aee52f
ipincamp <support@nur-arifin.my.id>
2024-11-13 07:41:16 +0700

    Add API routes for testing and login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+/**
+ * API Routes
+ */
+$routes->get('/test', 'AuthController::testAPI');
+$routes->post('/login', 'AuthController::login');
